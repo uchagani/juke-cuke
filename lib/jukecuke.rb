1191860a9e024c73ea2004d436e0342ec18e61a4
uchagani <umair.chagani@gmail.com>
2014-03-03 23:50:21 -0500

    adding requires

--- lib/jukecuke.rb
+++ lib/jukecuke.rb
@@ -1,4 +1,8 @@
 require "jukecuke/version"
+require "cucumber/cli/main"
+require 'jukecuke'
+require 'jukecuke/scenarios'
+require 'jukecuke/formatters/scenario_line_logger'
 
 module Jukecuke
   # Your code goes here...
